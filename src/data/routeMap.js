6aa424ca8240fc931a1ca4bba58db646f490a208
Sarah Schwartz <seschwartz8@gmail.com>
2020-04-16 12:01:17 -0700

    update projectData

---
+++ src/data/routeMap.js
@@ -0,0 +1,19 @@
+const routeMap = [
+  {
+    label: 'About',
+    id: 1,
+    path: '/about/',
+  },
+  {
+    label: 'Projects',
+    id: 2,
+    path: '/projects/',
+  },
+  {
+    label: 'Contact',
+    id: 3,
+    path: '/contact/',
+  },
+];
+
+export default routeMap;
