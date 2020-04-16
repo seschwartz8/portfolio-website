78892f14139cdb4804f56ef025b7e5bfeb2c82a0
Sarah Schwartz <seschwartz8@gmail.com>
2020-04-16 11:51:41 -0700

    extract icon and navbar components from app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import './styles/main.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Landing from './components/Landing';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+import Hamburger from './components/Hamburger';
+import Navbar from './components/Navbar';
+
+import githubIcon from './images/github-icon.png';
+import emailIcon from './images/email-icon.png';
+import resumeIcon from './images/resume-icon.png';
+import resume from './images/resume.pdf';
+import Icon from './components/Icon';
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename='/'>
+        <div id='app'>
+          <div id='nav'>
+            <div className='name'>
+              <a href='/' component={Landing} name='name-anchor'>
+                <h2>Sasa Schwartz</h2>
+              </a>
+            </div>
+            <Hamburger />
+            <Navbar />
+            <Icon
+              anchor={
+                <a href='https://github.com/seschwartz8' target='_blank'>
+                  <img src={githubIcon} id='github' width='40px' />
+                </a>
+              }
+            />
+            <Icon
+              anchor={
+                <a target='_blank' href={resume}>
+                  <img src={resumeIcon} alt='resume' width='40px' />
+                </a>
+              }
+            />
+            <Icon
+              anchor={
+                <a href='mailto: seschwartz8@gmail.com'>
+                  <img src={emailIcon} alt='email' width='40px' />
+                </a>
+              }
+            />
+          </div>
+          <Route exact path='/' component={Landing} />
+          <Route path='/about' component={About} />
+          <Route path='/projects' component={Projects} />
+          <Route path='/contact' component={Contact} />
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
