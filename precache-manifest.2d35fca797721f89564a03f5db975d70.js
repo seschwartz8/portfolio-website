623ad127a3511d505b940c1f19b8841f8558ef30
Sarah Schwartz <seschwartz8@gmail.com>
2020-04-22 11:14:24 -0700

    Updates

---
+++ precache-manifest.2d35fca797721f89564a03f5db975d70.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d150bd7aefab6f7179b7460d63ff7503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fe162dbc1dafd0f15d1",
+    "url": "/static/css/main.9680e12f.chunk.css"
+  },
+  {
+    "revision": "0f2511a43f884af2cc0f",
+    "url": "/static/js/2.6ac3f833.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6ac3f833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe162dbc1dafd0f15d1",
+    "url": "/static/js/main.f6a6441e.chunk.js"
+  },
+  {
+    "revision": "8dd1ec03cd7bd48089e8",
+    "url": "/static/js/runtime-main.a5b7bec9.js"
+  },
+  {
+    "revision": "9ed1e78c499b406cf6b91f49535e591e",
+    "url": "/static/media/apocalypse-crop.9ed1e78c.png"
+  },
+  {
+    "revision": "ccdb0784dd3ad26aa346da9a64a31add",
+    "url": "/static/media/blackjack-crop.ccdb0784.png"
+  },
+  {
+    "revision": "63f6de26728f7a7fae153fb5b619b229",
+    "url": "/static/media/githubbattle-crop.63f6de26.png"
+  },
+  {
+    "revision": "735fa4c740f91f3dcab422364c15eb10",
+    "url": "/static/media/pinterest-clone-crop.735fa4c7.png"
+  },
+  {
+    "revision": "5e42338281b206c4ffce8c2ff74a3a7b",
+    "url": "/static/media/portfolio-crop.5e423382.png"
+  },
+  {
+    "revision": "be414c5ca48109683963cd46f40a3e6b",
+    "url": "/static/media/portrait.be414c5c.jpg"
+  },
+  {
+    "revision": "8eafdb26cf1548ec5628d3191bee1057",
+    "url": "/static/media/resume.8eafdb26.pdf"
+  },
+  {
+    "revision": "43c92e837defe8dcef8021b55e9d16b0",
+    "url": "/static/media/typing-game-crop.43c92e83.png"
+  }
+]);
